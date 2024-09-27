dd44fc6b23d61097c0806f44436a2b4075a9f6aa
rigo72 <mattia.gori27@gmail.com>
2024-09-27 02:33:04 -0500

    Commit 1

---
+++ esercizio9.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $colore = "red";
+        $nome = "Gabriel";
+        echo "<h1 style='color:$colore;'>{$nome}</h1>"
+    ?>
+</body>
+</html>
